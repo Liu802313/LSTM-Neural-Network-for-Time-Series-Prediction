229fc9251b7dc609f0f40d7c21f9a870ea096e18
刘明 <liuming@liumingdeMacBook-Pro.local>
2018-10-10 10:06:59 +0800

    Get average price for every 600 ticks

---
+++ test/1.py
@@ -0,0 +1,32 @@
+#!/usr/bin/env python
+
+import pandas as pd
+import numpy as np
+import os
+
+fileList = []
+print(os.getcwd())
+for i in os.listdir(os.getcwd()):
+    #if(os.path.isfile(filepath)):
+    print(i)
+    if(os.path.splitext(i)[1] ==".csv"):
+        fileList.append(i)
+print(fileList)
+for i in fileList:
+    dataframe = pd.read_csv(i,header=None)
+
+    data_train = dataframe.get([10,19]).values
+    print(type(data_train))
+    print(type(dataframe))
+    data_train_new = []
+    for i in range(int(len(data_train)/600)):
+        data_train_new.append(data_train[600*i])
+
+    data_train_new = np.array(data_train_new)
+
+    for i in range(len(data_train_new)-1,0,-1):
+        data_train_new[i] = data_train_new[i] - data_train_new[i-1]
+        if(data_train_new[i,0] != 0):
+            data_train_new[i,1] = data_train_new[i,1] / data_train_new[i,0]
+    data_train_new = np.delete(data_train_new,0,axis=0)
+    print(data_train_new)
